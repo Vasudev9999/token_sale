4c3aca309c5b3f92bd07e21881439681b3ea0734
Vasudev Patel <22it118@charusat.edu.in>
2025-02-26 17:41:50 +0530

    3. Delegated Transfer

--- contracts/VasuToken.sol
+++ contracts/VasuToken.sol
@@ -7,8 +7,10 @@ contract VasuToken {
     uint256 public totalSupply;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
 
     function VasuToken(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -17,45 +19,35 @@ contract VasuToken {
 
     function transfer(address _to, uint256 _value) public returns (bool success) {
         if (balanceOf[msg.sender] < _value) {
-            throw; // Instead of require
+            throw; // Solidity 0.4.2 style
         }
 
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
-        Transfer(msg.sender, _to, _value); // Solidity <0.4.21 doesn't use 'emit'
+        Transfer(msg.sender, _to, _value); // No 'emit' in Solidity 0.4.2
+        return true;
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value); // Event without 'emit'
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        if (balanceOf[_from] < _value) {
+            throw;
+        }
+        if (allowance[_from][msg.sender] < _value) {
+            throw;
+        }
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        allowance[_from][msg.sender] -= _value;
+
+        Transfer(_from, _to, _value);
         return true;
     }
 }
-
-// pragma solidity ^0.4.2;
-
-// contract VasuToken {
-//     string public name = "Vasu Token";
-//     string public symbol = "VASU";
-//     string public standard = "Vasu Token v1.0";
-//     uint256 public totalSupply;
-
-//     event Transfer(
-//         address indexed _from,
-//         address indexed _to,
-//         uint256 _value
-//     );
-
-//     mapping(address => uint256) public balanceOf;
-
-//     function VasuToken(uint256 _initialSupply) public {
-//         balanceOf[msg.sender] = _initialSupply;
-//         totalSupply = _initialSupply;
-//     }
-
-//     function transfer(address _to, uint256 _value) public returns (bool success) {
-//         require(balanceOf[msg.sender] >= _value);
-
-//         balanceOf[msg.sender] -= _value;
-//         balanceOf[_to] += _value;
-
-//         Transfer(msg.sender, _to, _value);
-//         return true;
-//     }
-// }
--- test/VasuToken.js
+++ test/VasuToken.js
@@ -1,71 +1,106 @@
 var VasuToken = artifacts.require("./VasuToken.sol");
 
-contract("VasuToken", function(accounts) {
+contract('VasuToken', function(accounts) {
   var tokenInstance;
 
-  it("initializes the contract with the correct values", function() {
-    return VasuToken.deployed()
-      .then(function(instance) {
-        tokenInstance = instance;
-        return tokenInstance.name();
-      })
-      .then(function(name) {
-        assert.equal(name, "Vasu Token", "has the correct name");
-        return tokenInstance.symbol();
-      })
-      .then(function(symbol) {
-        assert.equal(symbol, "VASU", "has the correct symbol");
-        return tokenInstance.standard();
-      })
-      .then(function(standard) {
-        assert.equal(standard, "Vasu Token v1.0", "has the correct standard");
-      });
+  it('initializes the contract with the correct values', function() {
+    return VasuToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.name();
+    }).then(function(name) {
+      assert.equal(name, 'Vasu Token', 'has the correct name');
+      return tokenInstance.symbol();
+    }).then(function(symbol) {
+      assert.equal(symbol, 'VASU', 'has the correct symbol');
+      return tokenInstance.standard();
+    }).then(function(standard) {
+      assert.equal(standard, 'Vasu Token v1.0', 'has the correct standard');
+    });
   });
 
-  it("allocates the initial supply upon deployment", function() {
-    return VasuToken.deployed()
-      .then(function(instance) {
-        tokenInstance = instance;
-        return tokenInstance.totalSupply();
-      })
-      .then(function(totalSupply) {
-        assert.equal(totalSupply.toNumber(), 1000000, "sets the total supply to 1,000,000");
-        return tokenInstance.balanceOf(accounts[0]);
-      })
-      .then(function(adminBalance) {
-        assert.equal(adminBalance.toNumber(), 1000000, "allocates initial supply to admin");
-      });
+  it('allocates the initial supply upon deployment', function() {
+    return VasuToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.totalSupply();
+    }).then(function(totalSupply) {
+      assert.equal(totalSupply.toNumber(), 1000000, 'sets the total supply to 1,000,000');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(adminBalance) {
+      assert.equal(adminBalance.toNumber(), 1000000, 'allocates the initial supply to the admin account');
+    });
   });
 
-  it("transfers token ownership", function() {
-    return VasuToken.deployed()
-      .then(function(instance) {
-        tokenInstance = instance;
-        return tokenInstance.transfer.call(accounts[1],  9999999999999999);
-      })
-      .then(assert.fail)
-      .catch(function(error) {
-        assert(error, "Expected an error but did not get one"); // Fix here
-        return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
-      })
-      .then(function(success) {
-        assert.equal(success, true, "returns true");
-        return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
-      })
-      .then(function(receipt) {
-        assert.equal(receipt.logs.length, 1, "triggers one event");
-        assert.equal(receipt.logs[0].event, "Transfer", "should be the 'Transfer' event");
-        assert.equal(receipt.logs[0].args._from, accounts[0], "logs sender");
-        assert.equal(receipt.logs[0].args._to, accounts[1], "logs receiver");
-        assert.equal(receipt.logs[0].args._value, 250000, "logs amount");
-        return tokenInstance.balanceOf(accounts[1]);
-      })
-      .then(function(balance) {
-        assert.equal(balance.toNumber(), 250000, "adds amount to receiver");
-        return tokenInstance.balanceOf(accounts[0]);
-      })
-      .then(function(balance) {
-        assert.equal(balance.toNumber(), 750000, "deducts amount from sender");
-      });
+  it('transfers token ownership', function() {
+    return VasuToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+    }).then(assert.fail).catch(function(error) {
+      assert(error, 'Expected an error but did not get one'); 
+      return tokenInstance.transfer.call(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.transfer(accounts[1], 250000, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      return tokenInstance.balanceOf(accounts[1]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 250000, 'adds the amount to the receiving account');
+      return tokenInstance.balanceOf(accounts[0]);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 750000, 'deducts the amount from the sending account');
+    });
+  });
+
+  it('approves tokens for delegated transfer', function() {
+    return VasuToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      return tokenInstance.approve.call(accounts[1], 100);
+    }).then(function(success) {
+      assert.equal(success, true, 'it returns true');
+      return tokenInstance.approve(accounts[1], 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Approval', 'should be the "Approval" event');
+      return tokenInstance.allowance(accounts[0], accounts[1]);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 100, 'stores the allowance for delegated transfer');
+    });
+  });
+
+  it('handles delegated token transfers', function() {
+    return VasuToken.deployed().then(function(instance) {
+      tokenInstance = instance;
+      fromAccount = accounts[2];
+      toAccount = accounts[3];
+      spendingAccount = accounts[4];
+
+      return tokenInstance.transfer(fromAccount, 100, { from: accounts[0] });
+    }).then(function(receipt) {
+      return tokenInstance.approve(spendingAccount, 10, { from: fromAccount });
+    }).then(function(receipt) {
+      return tokenInstance.transferFrom(fromAccount, toAccount, 9999, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error, 'Expected an error but did not get one'); 
+      return tokenInstance.transferFrom(fromAccount, toAccount, 20, { from: spendingAccount });
+    }).then(assert.fail).catch(function(error) {
+      assert(error, 'Expected an error but did not get one'); 
+      return tokenInstance.transferFrom.call(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(success) {
+      assert.equal(success, true);
+      return tokenInstance.transferFrom(fromAccount, toAccount, 10, { from: spendingAccount });
+    }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, 'triggers one event');
+      assert.equal(receipt.logs[0].event, 'Transfer', 'should be the "Transfer" event');
+      return tokenInstance.balanceOf(fromAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 90, 'deducts the amount from the sending account');
+      return tokenInstance.balanceOf(toAccount);
+    }).then(function(balance) {
+      assert.equal(balance.toNumber(), 10, 'adds the amount to the receiving account');
+      return tokenInstance.allowance(fromAccount, spendingAccount);
+    }).then(function(allowance) {
+      assert.equal(allowance.toNumber(), 0, 'deducts the amount from the allowance');
+    });
   });
 });
